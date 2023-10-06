7a0f7648064876326a8be9fb488e47221b86fe3d
shmzzzz <slt0623@gmail.com>
2023-10-06 09:29:27 +0900

    FiltersScreenのリファクタリング

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// import 'package:meals/screens/tabs.dart';
+// import 'package:meals/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
@@ -16,6 +18,16 @@ class _FiltersScreenState extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text('Your Filters'),
       ),
+      // drawer: MainDrawer(onSelectScreen: (identifier) {
+      //   Navigator.of(context).pop();
+      //   if (identifier == 'meals') {
+      //     Navigator.of(context).pushReplacement(
+      //       MaterialPageRoute(
+      //         builder: (ctx) => const TabsScreen(),
+      //       ),
+      //     );
+      //   }
+      // }),
       body: Column(
         children: [
           SwitchListTile(
