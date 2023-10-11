fcbdf2ca6550d0cb56fb588bc731f7f01745ba68
shmzzzz <slt0623@gmail.com>
2023-10-11 09:37:13 +0900

    importするパッケージの修正

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -1,5 +1,5 @@
 import 'package:meals/models/meal.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
   FavoriteMealsNotifier() : super([]);
