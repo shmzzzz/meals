090f08ebdfb057b0f78eca115ca3ad0f629a2e52
shmzzzz <slt0623@gmail.com>
2023-10-15 11:56:18 +0900

    暗黙的アニメーションの作成

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -33,8 +33,17 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
-          )
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: animation,
+                  child: child,
+                );
+              },
+              child: Icon(isFavorite ? Icons.star : Icons.star_border),
+            ),
+          ),
         ]),
         body: SingleChildScrollView(
           child: Column(
